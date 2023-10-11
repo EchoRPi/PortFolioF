8dc3b1e201efa8cb42cde625fc77c6c02956b35e
EchoRPi <142438072+EchoRPi@users.noreply.github.com>
2023-10-11 08:44:49 +0200

    Revert "Correcion navbar y añadir ruta admin"
    This reverts commit 36971a2e53b63758d95781f457ac4a9cb2c0aaed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'proyecto-angular';
-  constructor(private router: Router) { }
-  isAdminRoute(): boolean {
-    return this.router.url === '/admin';
-  }
 }
